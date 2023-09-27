44c5f557facef6d0ccc43555c02f47eb29a49772
angelin-ust <127503097+angelin-ust@users.noreply.github.com>
2023-09-27 17:06:57 +0530

    Update streamlit_app.py
    123 added

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # new session
+streamlit.header('123')
 streamlit.header('fruityvice Fruit Advice')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
